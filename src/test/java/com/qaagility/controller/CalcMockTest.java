1d17eea8d5d88c5031d030d549506da489d79a42
Ashish <ash1174@gmail.com>
2018-03-26 15:49:24 +0530

    Update CalcMockTest.java

--- src/test/java/com/qaagility/controller/CalcMockTest.java
+++ src/test/java/com/qaagility/controller/CalcMockTest.java
@@ -1,37 +1,15 @@
 package com.qaagility.controller;
 
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
  
 public class CalcMockTest {
 
-      public void testSlow() {
-        System.out.println("slow");
-      }
+	@Mock
+    	MyCalc calcMock;	
 
-      public void testSlower() {
-        System.out.println("slower");
-      }
-
-      public void testFast() {
-        System.out.println("fast-updated");
-      }
-
-	@Test	
-      public void testCalc() {
-        assertEquals("Result", 9, 9);
-      }
-
-	@Test	
-      public void testCalc1() {
-        assertEquals("Result - Fail", 9, 9);
-      }
 	
 	@Test
 	public void testCalcMock1()  {
-	@Mock
-    	MyCalc calcMock;	
 		
         //  create mock
 	ClassToTest test  = new ClassToTest(calcMock);	
@@ -42,7 +20,7 @@ public class CalcMockTest {
 
         // use mock in test....
         assertEquals(test.getMul(), 81);
-}
+	}
 
 	
 	
