2188427d29335e90ec784d585a6c2b56010e041a
Ashish <ash1174@gmail.com>
2018-03-27 14:39:23 +0530

    Update CalcMockTest.java

--- src/test/java/com/qaagility/controller/CalcMockTest.java
+++ src/test/java/com/qaagility/controller/CalcMockTest.java
@@ -31,17 +31,18 @@ public class CalcMockTest {
 		//calcMock = mock(Calculator.class);
 	    }	
 
-/*	@Test
+	@Test
 	public void testCalcMock1()  {
 	System.out.println("Stubbing mul(x,y)"); 	
 	when(calcMock.mul(9,9)).thenReturn(81);
 	assertEquals(81,calcMock.mul(9,9));		
+	verify(calcMock, atLeastOnce()).mul(9,9);	
 	when(calcMock.add(6,3)).thenCallRealMethod();	
 	assertEquals(9,calcMock.add(6,3));		
 	verify(calcMock, atLeastOnce()).add(6,3);
         System.out.println("Verifying mul method is called atleast once");	
 	}
-*/	
+	
 	@Test
 	public void testCalcSpy1()  {
 	System.out.println("Spying mul(x,y)"); 	
